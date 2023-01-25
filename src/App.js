8abde74f2a5203d0b945f6c66bf4918390d679e4
Om Prakash Tiwari <optiwari@q3tech.com>
2023-01-25 15:21:48 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home';
-import Header from './Components/Header/Header';
-import AddExpense from './Components/Add Expense/AddExpense';
+import "./App.css";
 
 function App() {
   return (
-    // <div >
-    //  <div>
-    //   <Header/>
-    //  </div>
-    //  <div>
-    //   <Home/>
-    //  </div>
-    //  <div>
-    //   <h2>Footer</h2>
-    //  </div>
-
-    // </div>
-    <BrowserRouter>
-    <Routes>
-      
-      <Route path='/'  element={<Header/>}>
-        <Route index element={<Home/>}/>
-        <Route path='/add-expense' element={<AddExpense/>}/>
-      </Route>
-
-      
-
-    </Routes>
-    </BrowserRouter>
+    <>
+      <h1>eoirgh</h1>
+    </>
   );
 }
 
--- src/Components/ExpenseList/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Card = ({item}) => {
-  return (
-    <div>This is Card</div>
-  )
-}
-
-export default Card
